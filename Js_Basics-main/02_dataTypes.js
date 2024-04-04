b7195bf4975c9291746df49008c84c19a0c02bd5
Shahria Kabir <shahriakabir@Kabirs-Mac.local>
2024-04-04 15:05:16 +0600

    Dates

---
+++ Js_Basics-main/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //i am using nodejs, not browser
+
+// console.log(3+3); console.log("Kabir"); //not readability
+
+/*console.log(3+3); 
+                        //code readability; go to ECMA SCRIPT documentation, mdn also
+console.log("Kabir");*/
+
+// Datatypes
+
+let name = "Kabir"   //string
+let age = 23         //number
+let isLoggedIn = false      //boolean
+let state;    //undefined
+
+//number
+//string
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof "Kabir")             //to know the variable type
+
+//null => in console, null is object
+
---
+++ Js_Basics-main/07_datesinJS.js
@@ -0,0 +1,23 @@
+//Dates
+let myDate = new Date();
+console.log(myDate);
+
+console.log(myDate.toString());
+//Thu Apr 04 2024 14:43:06 GMT+0600 (Bangladesh Standard Time)
+
+console.log(myDate.toDateString());
+//Thu Apr 04 2024
+
+console.log(myDate.toISOString());
+//2024-04-04T08:46:14.919Z
+
+console.log(myDate.toJSON());
+//2024-04-04T08:46:49.720Z
+
+console.log(myDate.toLocaleDateString());
+// 4/4/2024
+
+console.log(myDate.toLocaleString());
+// 4/4/2024, 2:48:06 PM
+
+console.log(typeof myDate); //for interview
